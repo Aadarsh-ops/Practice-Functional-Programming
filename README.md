df6de26d172c737ae0765720d8dd04ccb4ca575f
addishbourne7 <paradisegamer649@gmail.com>
2019-08-28 17:27:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-weaxzp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-weaxzp)
---
+++ index.js
@@ -0,0 +1,39 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+//  Exercise 25
+const data = [
+	{
+		"name": "New Releases",
+		"videos": [
+			{
+				"id": 65432445,
+				"title": "The Chamber"
+			},
+			{
+				"id": 675465,
+				"title": "Fracture"
+			}
+		]
+	},
+	{
+		"name": "Thrillers",
+		"videos": [
+			{
+				"id": 70111470,
+				"title": "Die Hard"
+			},
+			{
+				"id": 654356453,
+				"title": "Bad Boys"
+			}
+		]
+	}
+]
+ 
+console.log(data)
